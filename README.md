0c240065b420ae40fd779c7a1cfd9118e92f5de5
kudarisenmon <kudarisenmon@gmail.com>
2021-09-09 16:47:07 +0900

    XML整形を追加

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # java-exif2gpx
+Create a GPX format track file from multiple Jpeg photos with Exif containing location information.
--- src/main/java/com/jpn/senmon/exif2gpx/App.java
+++ src/main/java/com/jpn/senmon/exif2gpx/App.java
@@ -1,10 +1,27 @@
 package com.jpn.senmon.exif2gpx;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import io.jenetics.jpx.GPX;
 import io.jenetics.jpx.Track;
@@ -12,7 +29,7 @@ import io.jenetics.jpx.TrackSegment;
 import io.jenetics.jpx.WayPoint;
 
 /**
- * Hello world!
+ * exif2gpx
  *
  */
 public class App {
@@ -38,8 +55,33 @@ public class App {
 			TrackSegment segment = TrackSegment.builder().points(efv.getWayPointList()).build();
 			Track track = Track.builder().name("Track 1").desc("Create by exif2gpx").addSegment(segment).build();
 			GPX gpx = GPX.builder().addTrack(track).build();
-			GPX.write(gpx, System.out);
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			GPX.write(gpx, baos);
+
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(new ByteArrayInputStream(baos.toByteArray()));
+			doc.setXmlStandalone(true);
+
+			Transformer tf = TransformerFactory.newInstance().newTransformer();
+			tf.setOutputProperty(OutputKeys.METHOD, "xml");
+			tf.setOutputProperty(OutputKeys.INDENT, "yes");
+			tf.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+			tf.transform(new DOMSource(doc), new StreamResult(System.out));
+
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
 			e.printStackTrace();
 		}
 	}
